b77c98b0c99400b4941c34cbc5472d64f344ef03
Jeffvo <jeffrey.oosterwijk@hotmail.com>
2025-03-18 20:06:13 +0000

    Validate webhook setup

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+	http.HandleFunc("/webhook", webhookHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func webhookHandler(w http.ResponseWriter, r *http.Request) {
+	// Check if the request is a POST request
+	if r.Method != "POST" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	// Read the request body
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Printf("Received a webhook: %s", string(body))
+
 }
