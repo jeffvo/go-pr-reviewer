bfb8683f34816860c620a4c51e5f5f9f25e0bf20
Jeffvo <jeffrey.oosterwijk@hotmail.com>
2025-05-07 19:55:35 +0100

    Ensure logging occurs

--- internal/usecases/webhook.go
+++ internal/usecases/webhook.go
@@ -1,6 +1,8 @@
 package usecases
 
 import (
+	"fmt"
+
 	"github.com/jeffvo/go-pr-reviewer/domain/ports"
 	"github.com/jeffvo/go-pr-reviewer/internal/usecases/dto"
 )
@@ -18,22 +20,26 @@ func (p *WebhookProcessor) ProcessWebhook(payload dto.WebhookPayload) error {
 
 	pullRequestFiles, err := p.gitService.GetPullRequest(payload.PullRequest.URL)
 	if err != nil {
+		fmt.Printf("Error getting pull request: %v\n", err)
 		return err
 	}
 
 	metadata, err := p.gitService.GetPullRequestMetadata(payload.PullRequest.URL)
 	if err != nil {
+		fmt.Printf("Error getting pull request metadata: %v\n", err)
 		return err
 	}
 
 	suggestions, err := p.aiService.GetCodeSuggestions(pullRequestFiles)
 
 	if err != nil {
+		fmt.Printf("Error getting code suggestions: %v\n", err)
 		return err
 	}
 
 	err = p.gitService.PostCodeSuggestions(payload.PullRequest.URL, suggestions, metadata)
 	if err != nil {
+		fmt.Printf("Error posting code suggestions: %v\n", err)
 		return err
 	}
 
